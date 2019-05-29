56f7e4a2f073d9115495e13bb1530945281b2368
HzKharel <43970402+HzKharel@users.noreply.github.com>
2019-05-29 16:48:55 +0100

    ui canges

--- src/components/layout/pages/Appearance.jsx
+++ src/components/layout/pages/Appearance.jsx
@@ -16,8 +16,8 @@ export default class Biography extends Component{
         return (
             <div>
                 <div className="row">
-                 <h1 className = "h1-title-style " style = {{fontWeight : 400}}>{this.state.name}</h1>
                     <div className = "column" style = {{height: '90%' , width: '50%'}}>
+                    <h1 className = "h1-title-style " style = {{fontWeight : 400}}>{this.state.name}</h1>
                     <h2 className = "h1-title-style">Gender : {this.state.appear['gender']}</h2> 
                     <h2 className = "h1-title-style">Race : {this.state.appear['race']}</h2>
                     <h2 className = "h1-title-style">height : {this.state.appear['height'].join('  /  ')}</h2>
@@ -25,8 +25,11 @@ export default class Biography extends Component{
                     <h2 className = "h1-title-style">Eye Colour : {this.state.appear['eye-color']}</h2>
                     <h2 className = "h1-title-style">Hair Colour : {this.state.appear['hair-color']}</h2>
                     </div>
-                    <div className = "column" style = {{height: '90%' , width: '40%', marginRight :'5%', alignItems : 'center'}}>
-                        <img src={this.state.image.url} style= {{height: '80%', width : '80%', marginRight :'5%'}} alt="None Available" ></img>
+                    <div className = "column" style = {{height: '90%' , width: '40%', marginRight :'5%',float: 'right'}}>
+                        <div style= {{float: 'right'}}>
+                            <img src={this.state.image.url} style= {{height: '65%', width : '65%', marginRight :'5%', float: 'right'}} alt="None Available" ></img>
+                        </div>
+                      
                         
                     </div>
                 </div>
